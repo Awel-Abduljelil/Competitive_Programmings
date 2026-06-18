00e171bde42ab7f9ace5465c33fca6ace64d96fa
Awel Abduljelil <awela1499@gmail.com>
2026-06-18 22:01:35 +0300

    Delete 4280-count-digit-appearances directory

--- 4280-count-digit-appearances/count-digit-appearances.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def countDigitOccurrences(self, nums: list[int], digit: int) -> int:
-        listToString = ""
-        for i in nums:
-            listToString += str(i)
-        return listToString.count(str(digit))

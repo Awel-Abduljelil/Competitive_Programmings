ec400d641fa7270779cf47a1c1aebdd5d88c1360
Awel Abduljelil <awela1499@gmail.com>
2026-07-02 22:45:20 +0300

    Delete 3846-minimum-operations-to-make-array-sum-divisible-by-k directory

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def minOperations(self, nums: List[int], k: int) -> int:
-        arr_sum = sum(nums)
-        count = 0
-        while not arr_sum%k == 0:
-            arr_sum -= 1
-            count += 1
-        return count

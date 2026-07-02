c9561356e8ed701303ab9180b529dbecfca9aa8f
Awel Abduljelil <awela1499@gmail.com>
2026-07-02 22:45:00 +0300

    Delete 2812-find-the-maximum-achievable-number directory

--- 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def theMaximumAchievableX(self, num: int, t: int) -> int:
-        x = t
-        for i in range(t):
-            x -= 1
-            num += 1
-        x = num + t
-        return x

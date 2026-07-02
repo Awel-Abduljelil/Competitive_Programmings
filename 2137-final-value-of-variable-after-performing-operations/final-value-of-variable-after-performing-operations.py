0a051445acdff8884c1784001c70cbc24c885ae5
Awel Abduljelil <awela1499@gmail.com>
2026-07-02 20:45:08 +0300

    Delete 2137-final-value-of-variable-after-performing-operations directory

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def finalValueAfterOperations(self, operations: List[str]) -> int:
-        X = 0
-        for string in operations:
-            if string == "--X"  or string == "X--":
-                X -= 1
-            else:
-                X += 1
-        return X
